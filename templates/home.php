be79c0260cc22ab44541e08823868cd668399b50
Eroto <seyive.awaya@gmail.com>
2024-04-13 15:54:57 +0100

    adding scroll animation

--- templates/home.php
+++ templates/home.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="/assets/css/style.css">
         <script src="https://cdn.tailwindcss.com"></script>
+        <script src="/assets/js/main.js"></script>
         <title>Home page</title>
     </head>
     <body class="bg-stone-900 h-screen">
@@ -12,7 +13,7 @@
         <header>
             <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 text-blue-600">
                 <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <a href="https://flowbite.com" class="flex items-center">
+                    <a href="" class="flex items-center">
                         <img src="/assets/clock-eleven-thirty-svgrepo-com.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
                         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">HM</span>
                     </a>
@@ -25,7 +26,7 @@
                             <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </button>
                     </div>
-                    <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                    <div class="justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                         <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <a href="#" class="block py-2 pr-4 pl-3 text-blue-600 rounded  lg:p-0 hover:text-blue-900" aria-current="page">Home</a>
@@ -64,7 +65,7 @@
             </div>
         </div>
         
-        <a href="/members" class="block m-auto tracking-widest text-center w-7 h-7 rounded hover:bg-orange-600 text-white ">Create</a>
+        <!-- <a href="/members" class="block m-auto tracking-widest text-center w-7 h-7 rounded hover:bg-orange-600 text-white ">Create</a> -->
         <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
             <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
             <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">HomeHandler</a>. All Rights Reserved.
