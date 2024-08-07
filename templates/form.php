f51e21de206901ec4d1f3d20f935ae9639cfb22c
Prunel Mel <seyive.awaya@gmail.com>
2024-08-07 04:29:25 +0100

    updating form

--- templates/form.php
+++ templates/form.php
@@ -11,12 +11,12 @@
         <title>Create a new member</title>
     </head>
     <body class="h-full h-screen">
-        <div class="container w-full grid grid-cols-3 content-end h-2/3 m-auto">
-            <div class="hidde">  
+        <div class="container w-full grid grid-cols-1 content-end h-2/3 m-auto">
+            <!--<div class="hidde">  
                 <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
 
                 <dotlottie-player src="https://lottie.host/8cd00f6e-0a3e-4411-bd63-efac37cc8b29/XrZv3de4vX.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></dotlottie-player>
-            </div>
+            </div>-->
             
             <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
                 <div class="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -29,21 +29,21 @@
                         <div>
                             <label for="email" class="block text-sm font-medium leading-6 text-blue-700">Name</label>
                             <div class="mt-2" >
-                                <input  id="email" name="email"   required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                <input  id="email" name="email"   type="email"  required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             </div>
                         </div>
 
-                        <!-- <div>
-                            <div class="flex items-center justify-between">
-                                <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                                <div class="text-sm">
-                                    <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-                                </div>
+                        
+                        <div class="flex items-center justify-between">
+                            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                            <div class="text-sm">
+                                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
                             </div>
-                            <div class="mt-2">
-                            <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                            </div>
-                        </div> -->
+                        </div>
+                        <div class="mt-2">
+                        <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                        <br>
 
                         <div>
                             <button type="submit" class="flex w-full justify-center rounded-md bg-blue-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</button>
@@ -58,29 +58,7 @@
             </div>
         </div>
 
-        <br>
-        <br><br>
-        <footer class="bg-white">
-            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span class="text-sm text-blue-700 sm:text-center font-mono font-bold">© 2023 <a href="https://flowbite.com/" class="hover:underline">HomeHandler</a>. All Rights Reserved.
-            </span>
-            <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-blue-700 font-mono font-bold sm:mt-0">
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
-            </ul>
-            </div>
-        </footer>
-
+        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     </body>
 </html>
